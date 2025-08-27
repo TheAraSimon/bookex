a8c116160646a256f0d987eeafccb3ef8742eed4
Ara Simonian <simonian.ara.simon@gmail.com>
2025-08-27 14:22:03 +0400

    Initial commit

---
+++ src/test/java/com/example/bookex/BookExchangeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookExchangeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
